07ec67045b6a18577a4ace1aff5de96c39fc9221
Vlad Chuchupak <vlad.c@awery.aero>
2022-07-29 21:19:48 +0200

    fix: explore section height

--- src/components/IndexPage/Explore.tsx
+++ src/components/IndexPage/Explore.tsx
@@ -8,7 +8,7 @@ const LottieExplore = dynamic(() => import("@/components/Lottie/Explore"), {
 
 const Explore: FC = () => {
     return (
-        <section className="flex flex-col md:flex-row items-center justify-center pt-10">
+        <section className="flex flex-col md:flex-row items-center justify-center pt-10 min-h-[400px]">
             <div className="max-w-xl">
                 <h2 className="text-5xl md:text-8xl dark:text-white m-0 text-center md:text-left">Explore.</h2>
                 <h3 className="text-3xl md:text-4xl dark:text-white m-0 text-center md:text-left font-normal mt-6 tracking-widest leading-6">Millions
