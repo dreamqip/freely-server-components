eab5e484530d3c2812f48c2072690bb966a5904b
Vlad Chuchupak <vlad.c@awery.aero>
2022-07-31 00:16:56 +0200

    fix: header logo path

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -50,7 +50,7 @@ const MainHeader: FC = () => {
     return (
         <Header
             className={headerClasses}>
-            <Link href="/src/pages">
+            <Link href="/">
                 <div className="flex cursor-pointer items-center gap-4">
                     <Image
                         src="/movie-glass.svg"
