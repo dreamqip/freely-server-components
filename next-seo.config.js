3d068ab8ba44a2bcd9a0024e697282a23023b02d
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2022-12-14 20:24:44 +0100

    feat: new pwa icons, seo config additions

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,39 +1,42 @@
 export default {
-	defaultTitle: "Freely",
-	description:
-		"Freely offers movie details and trailers. It also includes popular movies and reviews.",
-	additionalMetaTags: [
-		{
-			property: "keywords",
-			content:
-				"movie app, movie details, popular movies, upcoming movies",
-		},
-		{
-			name: "author",
-			content: "https://github.com/dreamqip",
-		},
-		{
-			name: "theme_color",
-			content: "#6200EE",
-		},
-		{
-			name: "viewport",
-			content: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-		}
-	],
-	additionalLinkTags: [
-		{
-			rel: "icon",
-			href: "/favicon.png",
-		},
-		{
-			rel: "apple-touch-icon",
-			href: "/icons/icon-192x192.png",
-			sizes: "192x192",
-		},
-		{
-			rel: "manifest",
-			href: "/manifest.json",
-		},
-	],
-}
+  defaultTitle: 'Freely',
+  description:
+    'Freely offers movie details and trailers. It also includes popular movies and reviews.',
+  additionalMetaTags: [
+    {
+      property: 'keywords',
+      content: 'movie app, movie details, popular movies, upcoming movies',
+    },
+    {
+      name: 'author',
+      content: 'https://github.com/dreamqip',
+    },
+    {
+      name: 'theme_color',
+      content: '#6200EE',
+    },
+    {
+      name: 'viewport',
+      content:
+        'minimum-scale=1, initial-scale=1, width=device-width, height=device-height, shrink-to-fit=no, viewport-fit=cover, maximum-scale=5',
+    },
+  ],
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: '/favicon.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/manifest.json',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/icons/apple-icon-180.png',
+    },
+    {
+      rel: 'apple-mobile-web-app-capable',
+      content: 'yes',
+    },
+  ],
+};
