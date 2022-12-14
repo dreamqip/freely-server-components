2c33ba8d0e0225a4f5428b39730baedf685cb016
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2022-12-14 21:02:01 +0100

    feat: actor profile pictures improvements, empty state

--- src/components/ActorPage/ImageList.tsx
+++ src/components/ActorPage/ImageList.tsx
@@ -1,44 +1,40 @@
-import { FC } from "react"
-import { IProfile } from "@/types/images"
-import { Empty, Image } from "antd"
+import type { FC } from 'react';
+import type { IProfile } from '@/types/images';
+import { Image } from 'antd';
+import { imageBaseUrlOriginal } from '@/services/themoviedb';
+import Empty from '@/components/Empty';
 
 interface ImageListProps {
-    images: IProfile[]
+  images: IProfile[];
 }
 
 const ImageList: FC<ImageListProps> = ({ images }) => {
-    if (!images) {
-        return (
-            <Empty description={""}>
-                <h3 className="text-xl dark:text-primary-dark">No images</h3>
-            </Empty>
-        )
-    }
+  if (!images) {
+    return <Empty description='No images found' />;
+  }
 
-    return (
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-            <Image.PreviewGroup>
-                {images &&
-                    images.map((image) => {
-                        return (
-                            <div
-                                className="min-h-[100px] md:min-h-[200px]"
-                                key={image.file_path}
-                            >
-                                <Image
-                                    rootClassName="flex h-full"
-                                    placeholder={true}
-                                    src={`https://image.tmdb.org/t/p/original${image.file_path}`}
-                                    alt="backdrop"
-                                    fallback="./fallback.jpeg"
-                                    loading="lazy"
-                                />
-                            </div>
-                        )
-                    })}
-            </Image.PreviewGroup>
-        </div>
-    )
-}
+  return (
+    <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+      <Image.PreviewGroup>
+        {images &&
+          images.map((image) => {
+            return (
+              <Image
+                key={image.file_path}
+                placeholder
+                rootClassName='rounded-md overflow-hidden'
+                className='aspect-[2/3]'
+                src={`${imageBaseUrlOriginal}${image.file_path}`}
+                alt='actor image'
+                fallback='./fallback.jpeg'
+                loading='lazy'
+                decoding='async'
+              />
+            );
+          })}
+      </Image.PreviewGroup>
+    </div>
+  );
+};
 
-export default ImageList
+export default ImageList;
