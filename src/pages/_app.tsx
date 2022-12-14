c0c3b9655b6e44deb66f659c02f52e4dc33b5a1c
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2022-12-14 20:19:49 +0100

    feat: new main font, script worker strategy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,60 @@
-import "@/styles/globals.css"
+import '@/styles/globals.css';
 
-import type { AppProps as NextAppProps } from "next/app"
-import { wrapper } from "../store"
-import dynamic from "next/dynamic"
-import { DefaultSeo } from "next-seo"
-import SEO from "../../next-seo.config"
-import { Provider } from "react-redux"
+import type { AppProps as NextAppProps } from 'next/app';
+import { wrapper } from '@/store';
+import { DefaultSeo } from 'next-seo';
+import { Provider } from 'react-redux';
+import SEO from '../../next-seo.config';
+import dynamic from 'next/dynamic';
+import { Montserrat } from '@next/font/google';
+import Script from 'next/script';
 
-const MainLayout = dynamic(() => import("@/layouts/MainLayout"))
-const Progress = dynamic(() => import("@/components/ProgressBar"))
+const MainLayout = dynamic(() => import('@/layouts/MainLayout'));
+const Progress = dynamic(() => import('@/components/ProgressBar'));
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+});
 
 type AppProps<P = any> = {
-    Component: NextAppProps<P>["Component"] & { theme?: string }
-    rest: any
-} & P
+  Component: NextAppProps<P>['Component'] & { theme?: string };
+  rest: any;
+} & P;
 
 function MyApp({ Component, ...rest }: AppProps) {
-    const { store, props } = wrapper.useWrappedStore(rest)
+  const { store, props } = wrapper.useWrappedStore(rest);
 
-    return (
-        <>
-            <DefaultSeo {...SEO} />
-            <Progress />
-            <Provider store={store}>
-                <MainLayout theme={Component.theme}>
-                    <Component {...props.pageProps} />
-                </MainLayout>
-            </Provider>
-        </>
-    )
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-montserrat: ${montserrat.style.fontFamily};
+        }
+      `}</style>
+      <Script
+        id='google-analytics'
+        src='https://www.googletagmanager.com/gtag/js?id=G-3FTM4H2WDP'
+        strategy='worker'
+        dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-3FTM4H2WDP');
+          `,
+        }}
+      ></Script>
+
+      <DefaultSeo {...SEO} />
+      <Progress />
+      <Provider store={store}>
+        <MainLayout theme={Component.theme}>
+          <Component {...props.pageProps} />
+        </MainLayout>
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
