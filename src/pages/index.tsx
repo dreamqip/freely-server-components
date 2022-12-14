4b90b9cf07fff6fd6aedf381ee0deb56869cd775
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2022-12-14 20:51:17 +0100

    feat: home new carousel's data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,67 @@
-import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next"
-import axios from "axios"
-import dynamic from "next/dynamic"
-import { Suspense } from "react"
-import Spinner from "@/components/Spinner"
-import Hero from "@/components/IndexPage/Hero"
-import Explore from "@/components/IndexPage/Explore"
-import Watch from "@/components/IndexPage/Watch"
+import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+import Hero from '@/components/IndexPage/Hero';
+import Explore from '@/components/IndexPage/Explore';
+import Watch from '@/components/IndexPage/Watch';
+import { wrapper } from '@/store';
+import {
+  getNowPlayingMovies,
+  getPopularTvShows,
+  getRunningQueriesThunk,
+  getTopRatedMovies,
+  getTopRatedSeries,
+  useGetNowPlayingMoviesQuery,
+  useGetPopularTvShowsQuery,
+  useGetTopRatedMoviesQuery,
+  useGetTopRatedSeriesQuery,
+} from '@/services/themoviedb';
+import Spinner from '@/components/Spinner';
 
 const ShowCarousel = dynamic(
-    () => import("@/components/ShowCarousel/ShowCarousel"),
-    {
-        suspense: true,
+  () => import('@/components/ShowCarousel/ShowCarousel'),
+  {
+    suspense: true,
+  }
+);
+
+const Home: NextPage = () => {
+  const { data: topRated } = useGetTopRatedMoviesQuery(1);
+  const { data: nowPlaying } = useGetNowPlayingMoviesQuery(1);
+  const { data: popularSeries } = useGetPopularTvShowsQuery(1);
+  const { data: topRatedSeries } = useGetTopRatedSeriesQuery(1);
+
+  return (
+    <>
+      <Hero />
+      <Explore />
+      <Watch />
+      {topRated && nowPlaying && popularSeries && topRatedSeries ? (
+        <Suspense fallback={<Spinner />}>
+          <ShowCarousel title='Latest Series' series={popularSeries} />
+          <ShowCarousel title='Now In Theaters' series={nowPlaying} />
+          <ShowCarousel title='Top Rated Series' series={topRatedSeries} />
+          <ShowCarousel title='Top Rated' series={topRated} />
+        </Suspense>
+      ) : null}
+    </>
+  );
+};
+
+export default Home;
+
+export const getStaticProps = wrapper.getStaticProps(
+  ({ dispatch }) =>
+    async () => {
+      dispatch(getTopRatedMovies.initiate(1));
+      dispatch(getNowPlayingMovies.initiate(1));
+      dispatch(getPopularTvShows.initiate(1));
+      dispatch(getTopRatedSeries.initiate(1));
+
+      await Promise.all(dispatch(getRunningQueriesThunk()));
+
+      return {
+        props: {},
+      };
     }
-)
-
-const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
-    popular,
-    topRated,
-    upcoming,
-    now_playing,
-}) => {
-    return (
-        <>
-            <Hero />
-            <Explore />
-            <Watch />
-            <Suspense fallback={<Spinner />}>
-                <ShowCarousel title="Now In Theaters" series={now_playing} />
-                <ShowCarousel title="Upcoming movies" series={upcoming} />
-                <ShowCarousel title="Popular now" series={popular} />
-                <ShowCarousel title="Top Rated" series={topRated} />
-            </Suspense>
-        </>
-    )
-}
-
-export default Home
-
-export const getStaticProps: GetStaticProps = async () => {
-    const popularMovies = await axios.get(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
-    )
-    const topRated = await axios.get(
-        `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
-    )
-    const upcoming = await axios.get(
-        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1`
-    )
-    const playing = await axios.get(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
-    )
-
-    return {
-        props: {
-            topRated: topRated.data,
-            popular: popularMovies.data,
-            upcoming: upcoming.data,
-            now_playing: playing.data,
-        },
-    }
-}
+);
