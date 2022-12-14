aa8a2f43df29125e9461ea17f26e631fb0a59684
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2022-12-14 21:01:14 +0100

    fix: duplicate roles

--- src/components/CastCarousel/CastList.tsx
+++ src/components/CastCarousel/CastList.tsx
@@ -1,45 +1,54 @@
-import type { IMovieCast } from "@/types/cast"
-import type { IMovieCredits } from "@/types/credits"
-import { FC, Suspense } from "react"
-import CastCard from "./CastCard"
-import { SwiperSlide } from "swiper/react"
-import dynamic from "next/dynamic"
+import type { IMovieCredits, ITvShowCredits } from '@/types/credits';
+import { FC, Suspense } from 'react';
+import CastCard from './CastCard';
+import { SwiperSlide } from 'swiper/react';
+import dynamic from 'next/dynamic';
+import Spinner from '@/components/Spinner';
+import Empty from '@/components/Empty';
 
-const Swiper = dynamic(() => import("../ShowCarousel/SwiperLazy"), {
-    suspense: true,
-})
+const Swiper = dynamic(() => import('../ShowCarousel/SwiperLazy'), {
+  suspense: true,
+});
 
-interface MoviesListProps {
-    credits: IMovieCredits
-    title: string
+interface CastListProps {
+  credits: ITvShowCredits | IMovieCredits;
+  title: string;
 }
 
-const CastList: FC<MoviesListProps> = ({ credits, title }) => {
-    return (
-        <div className="py-10">
-            <h2 className="text-center text-6xl font-bold dark:text-white">
-                {title}
-            </h2>
-            <Suspense
-                fallback={
-                    <div className="flex items-center justify-center text-4xl">
-                        Loading...
-                    </div>
+const CastList: FC<CastListProps> = ({ credits, title }) => {
+  return (
+    <div className='py-4 md:py-10'>
+      <h2 className='mb-4 text-center text-4xl font-bold dark:text-white md:text-6xl'>
+        {title}
+      </h2>
+      {credits.cast.length > 0 ? (
+        <Suspense fallback={<Spinner />}>
+          <Swiper>
+            {credits &&
+              credits.cast.map((person) => {
+                // check if array have person who performed two roles in the movie
+                // and if there is, then we will not show him in the cast list
+                // because it will be a duplicate
+                const isDuplicate = credits.cast.some(
+                  (item) =>
+                    item.name === person.name &&
+                    item.character !== person.character
+                );
+                if (!isDuplicate) {
+                  return (
+                    <SwiperSlide key={person.id}>
+                      <CastCard person={person} />
+                    </SwiperSlide>
+                  );
                 }
-            >
-                <Swiper>
-                    {credits &&
-                        credits.cast.map((person: IMovieCast) => {
-                            return (
-                                <SwiperSlide key={person.id}>
-                                    <CastCard person={person} />
-                                </SwiperSlide>
-                            )
-                        })}
-                </Swiper>
-            </Suspense>
-        </div>
-    )
-}
+              })}
+          </Swiper>
+        </Suspense>
+      ) : (
+        <Empty description='No cast found' />
+      )}
+    </div>
+  );
+};
 
-export default CastList
+export default CastList;
