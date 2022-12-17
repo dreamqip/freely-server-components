8ab36b9fdc3c150616c9c4a9c44f81a1252ac7cf
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2022-12-17 15:20:26 +0100

    feat: change footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,22 @@
 import type { FC } from 'react';
-import { CodeBracketIcon } from '@heroicons/react/24/solid';
-import { memo } from 'react';
+import { GithubFilled } from '@ant-design/icons';
 
 const MainFooter: FC = () => {
   return (
     <footer className='flex flex-0-auto items-center justify-center gap-8 bg-inherit py-6 px-12'>
       <div className='flex text-xl leading-8 dark:text-white'>
-        <CodeBracketIcon className='mr-2 h-8 w-8' />
-        by
         <a
-          className='ml-2 text-primary-500 underline dark:text-primary-dark'
-          href='https://github.com/dreamqip'
+          href='https://github.com/dreamqip/freely'
           target='_blank'
-          rel='noreferrer'
+          rel='noopener noreferrer'
+          className='flex items-center gap-2'
         >
-          dreamqip
+          <GithubFilled className='text-4xl' />
+          <span>GitHub</span>
         </a>
       </div>
     </footer>
   );
 };
 
-export default memo(MainFooter);
+export default MainFooter;
