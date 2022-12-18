c2369eada18fd788ec2980daedfe75eba72c5bd5
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2022-12-18 19:16:55 +0100

    feat: footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,18 +3,27 @@ import { GithubFilled } from '@ant-design/icons';
 
 const MainFooter: FC = () => {
   return (
-    <footer className='flex flex-0-auto items-center justify-center gap-8 bg-inherit py-6 px-12'>
-      <div className='flex text-xl leading-8 dark:text-white'>
+    <footer className='flex flex-0-auto flex-col items-center justify-between gap-4 bg-inherit py-6 px-12 text-lg sm:flex-row sm:gap-0'>
+      <div className='flex items-center'>
+        powered by
         <a
-          href='https://github.com/dreamqip/freely'
+          href='https://www.themoviedb.org/'
           target='_blank'
-          rel='noopener noreferrer'
-          className='flex items-center gap-2'
+          rel='noreferrer'
+          className='ml-1 text-blue-500'
         >
-          <GithubFilled className='text-4xl' />
-          <span>GitHub</span>
+          TMDB
         </a>
       </div>
+      <a
+        href='https://github.com/dreamqip/freely'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='flex items-center gap-2'
+      >
+        <GithubFilled />
+        <span>Source code</span>
+      </a>
     </footer>
   );
 };
